2c43d60f152bf2481c99ed413924096054ba9343
Maksim <Maksim@DESKTOP-8R2DHUR>
2024-11-16 18:40:41 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public AuthorService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Author> GetAuthor()
+        {
+            var booksWithAuthors = await _dbContext.Books
+                .Include(b => b.Author)
+                .ToListAsync();
+
+            var maxLength = booksWithAuthors.Max(b => b.Title.Length);
+
+            var authorsWithMaxLengthBooks = booksWithAuthors
+                .Where(b => b.Title.Length == maxLength)
+                .Select(b => b.Author)
+                .Distinct()
+                .ToList();
+
+            var authorWithMinId = authorsWithMaxLengthBooks
+                .OrderBy(a => a.Id)
+                .FirstOrDefault();
+
+            return authorWithMinId;
+        }
+
+        public async Task<List<Author>> GetAuthors()
+        {
+            return await _dbContext.Authors
+            .Where(author => _dbContext.Books
+                .Count(book => book.AuthorId == author.Id && book.PublishDate.Year > 2015) % 2 == 0)
+            .ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public BookService(ApplicationDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Book> GetBook()
+        {
+            return await _dbContext.Books
+            .OrderByDescending(book => book.Price * book.QuantityPublished)
+            .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            DateTime albumReleaseDate = new DateTime(2012, 5, 25);
+            return await _dbContext.Books
+                .Where(book => book.Title.Contains("Red") && book.PublishDate > albumReleaseDate)
+                .ToListAsync();
+        }
+    }
+}
